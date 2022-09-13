e26763acca415e81e2b851c209408d92394a15ed
Apreksha <60358930+Apreksha@users.noreply.github.com>
2022-09-14 02:15:04 +0530

    Update HomeScreen.dart

--- lib/MobileScreens/HomePage/HomeScreen.dart
+++ lib/MobileScreens/HomePage/HomeScreen.dart
@@ -1,8 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:xiaomi_hackathon/MobileScreens/CartScreen/cart.dart';
 import 'package:xiaomi_hackathon/MobileScreens/Profile.dart';
 import 'package:xiaomi_hackathon/MobileScreens/productDB.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'CategoriesWidget.dart';
 import 'HomeAppBar.dart';
 import 'ItemsWidget.dart';
@@ -16,11 +16,21 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int homeIndex=0;
+
   @override
   Widget build(BuildContext context) {
 
     double _h =MediaQuery.of(context).size.height;
     double _w = MediaQuery.of(context).size.width;
+
+    void _scan(){
+      setState(() async{
+        String _data = await FlutterBarcodeScanner.scanBarcode(
+            '#000000', "Cancel", true, ScanMode.BARCODE);
+        print('dataaaaaaaa.............. $_data');
+      });
+    }
+
     return FutureBuilder(
         future: productDB().productDetails(),
         builder: (context,snapshot){
@@ -31,29 +41,28 @@ class _HomeScreenState extends State<HomeScreen> {
               bottomNavigationBar: CurvedNavigationBar(
                 height: _h/15,
                 color: Colors.orange,
-                backgroundColor: Colors.transparent,
+                backgroundColor: Colors.grey.shade200,
                 items: <Widget>[
                   Icon(homeIndex == 0?Icons.home_outlined:Icons.home, size: 30),
                   Icon(homeIndex == 1? Icons.person_2_outlined:Icons.person, size: 30),
-                  Icon(Icons.logout, size: 30),
+                  const Icon(Icons.logout, size: 30),
                 ],
                 onTap: (index){
                   setState(() {
-                    print(index);
                     homeIndex=index;
                   });
                 },
                 animationCurve: Curves.fastLinearToSlowEaseIn,
               ),
 
-              body: homeIndex == 1?const Profile():ListView(
+              body: homeIndex == 1 ? const Profile():ListView(
                 children: [
-                  HomeAppBar(),
+                  const HomeAppBar(),
                   Container(
-                    padding: EdgeInsets.only(top: 15),
+                    padding: const EdgeInsets.only(top: 15),
                     decoration: BoxDecoration(
                         color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.only(
+                        borderRadius: const BorderRadius.only(
                             topLeft: Radius.circular(35),
                             topRight: Radius.circular(35)
                         )
@@ -61,8 +70,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Column(
                       children: [
                         Container(
-                          margin: EdgeInsets.symmetric(horizontal: 15),
-                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          margin: const EdgeInsets.symmetric(horizontal: 15),
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
                           height: _h/16,
                           decoration: BoxDecoration(
                             color: Colors.white,
@@ -70,20 +79,23 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                           child: Row(children: [
                             Container(
-                              margin: EdgeInsets.only(left: 5),
+                              margin: const EdgeInsets.only(left: 5),
                               height: 50,
                               width: 290,
                               child: TextFormField(
-                                decoration: InputDecoration(
+                                decoration: const InputDecoration(
                                   border: InputBorder.none,
                                   hintText: "Search here....",
                                 ),
                               ),
                             ),
-                            Spacer(),
-                            Icon(Icons.document_scanner_outlined,
-                              size: _w/18,
-                              color: Colors.orange,),
+                            const Spacer(),
+                            GestureDetector(
+                              onTap: ()=>_scan(),
+                              child: Icon(Icons.document_scanner_outlined,
+                                size: _w/18,
+                                color: Colors.orange,),
+                            ),
 
                           ],),
                         ),
@@ -100,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 color: Colors.orange
                             ),),
                         ),
-                        SizedBox(
+                        const SizedBox(
                             height: 50,
                             child: CategoriesWidget()),
                         Container(
@@ -116,7 +128,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
                         SizedBox(
                             height: _h,
-                            child: ItemsWidget())
+                            child: const ItemsWidget())
                       ],
                     ),
                   ),
@@ -124,7 +136,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             );
           }else{
-            return Scaffold(
+            return const Scaffold(
                 body: Center(child: CircularProgressIndicator()));
           }
         });
